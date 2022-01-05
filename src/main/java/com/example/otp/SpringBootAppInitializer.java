159935ea53ec8677b7f19af3e1ffbd00798ea0ff
pramuda <pramuda@misynergy.com>
2022-01-05 09:40:27 +0530

    first commit

---
+++ src/main/java/com/example/otp/SpringBootAppInitializer.java
@@ -0,0 +1,17 @@
+package com.example.otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 1/5/22
+ **/
+
+@SpringBootApplication
+public class SpringBootAppInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppInitializer.class);
+    }
+}
---
+++ src/main/java/com/example/otp/controller/CustomerFormController.java
@@ -0,0 +1,14 @@
+package com.example.otp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 1/5/22
+ **/
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerFormController {
+
+}
